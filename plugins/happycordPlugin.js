3a8e43bd1929cef8d24e6c01e12829fb3f1cc2fa
Happy enderman <66224387+happyendermangit@users.noreply.github.com>
2024-01-16 19:12:40 +0100

    Update happycordPlugin.js

--- plugins/happycordPlugin.js
+++ plugins/happycordPlugin.js
@@ -1,5 +1,18 @@
 window.happycordPlugin = {
   onLoad: (Happycord) => {
     Happycord.ShowToast("Hi! this is a plugin runned (onLoad)",4)
-  } 
+  },
+  Settings: (h) => {
+          return [
+              // children of modal
+              h.reactModules.jsx(window.Happycord.findByProps('Text').Text,{
+                  varient:"text-md/medium",
+                  children:"setttings of the example plugin"
+              }),
+              () => {
+                  // on confirm
+                  console.log('okay was pressed')
+              }
+          ]
+      }
 }
