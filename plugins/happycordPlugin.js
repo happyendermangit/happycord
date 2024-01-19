c79c661af618cac8debd12913c82df32f5aec4f3
Happy enderman <66224387+happyendermangit@users.noreply.github.com>
2024-01-19 14:16:08 +0100

    Update happycordPlugin.js

--- plugins/happycordPlugin.js
+++ plugins/happycordPlugin.js
@@ -1,3 +1,13 @@
+/** example plugin code for starters
+
+* events supported:
+  - onLoad (runs once CONNECTION_OPEN is dispatched)
+  - onDisable (runs once plugin is disabled)
+  - onDispatch (runs once a dispatch event is fired)
+
+ Settings is the main config for the plugin!!
+**/
+
 window.happycordPlugin = {
   onLoad: (Happycord) => {
     Happycord.ShowToast("Hi! this is a plugin runned (onLoad)",4)
